4ed9b511978f0b4667e13e75615a9483c6acb7fa
Lester Jan Artienda <artiele@oocl.com>
2019-09-11 20:16:30 +0800

    Start practice-2-1

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,30 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+  let out = [];
+  collection.forEach((cv,ci) => {
+    if(ci==0) out.push({"key":cv,"count":0}); // initialize object structure
+    out.find((ov,oi) => {
+      if (ov.key === cv) {
+        console.log("IF")
+        out[oi].count++
+      } else {
+        console.log("ELSE")
+      }
+    })
+  });
+  return out;
 }
+
+const collection = [
+  'a', 'a', 'a',
+  'e', 'e', 'e', 'e', 'e', 'e', 'e',
+  'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h',
+  't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
+  'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f',
+  'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c',
+  'g', 'g', 'g', 'g', 'g', 'g', 'g',
+  'b', 'b', 'b', 'b', 'b', 'b',
+  'd', 'd', 'd', 'd', 'd'
+];
+console.log(countSameElements(collection)) //  {key: 'a', count: 3},
