02011c794a950ecae13eef6296c57e17d0d50aed
Lester Jan Artienda <artiele@oocl.com>
2019-09-11 18:02:19 +0800

    Complete practice-1

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,21 @@
 'use strict';
 
+// function collectSameElements(collectionA, collectionB) {
+//   let out = [];
+//   const pool = collectionA.concat(collectionB).sort();
+//   const filtered = pool.map((v,i)=>{
+//     if (v==pool[i+1]) out.push(v);
+//     return;
+//   })
+//   return out;
+  
+// }
+
 function collectSameElements(collectionA, collectionB) {
-  return 'Implement the practice require, and begin changing code in this row';
-}
+  let out = [];
+  collectionA.forEach((av)=>{
+    collectionB.find((bv)=>{
+      if(av==bv) out.push(av);
+    })
+  })
+  return out;}
