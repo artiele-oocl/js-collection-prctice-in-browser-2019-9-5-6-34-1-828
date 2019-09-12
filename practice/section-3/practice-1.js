8df2e0a53c77ccde78939de7fb04b7736ce7b1f8
Lester Jan Artienda <artiele@oocl.com>
2019-09-12 20:10:34 +0800

    Start practice-3-1

--- practice/section-3/practice-1.js
+++ practice/section-3/practice-1.js
@@ -1,5 +1,29 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  const updatedCollection = collectionA.map((value)=>{
+    if(isInObject(objectB,value.key)) return value.count--;
+    return {"key":value.key,"count":value.count}
+  })
+  return updatedCollection;
 }
+
+function isInObject(object,val){
+  console.log(object.value)
+  return object.value.indexOf(val) > -1
+}
+
+const collectionA = [
+  {key: 'a', count: 2},
+  {key: 'e', count: 2},
+  {key: 'h', count: 2},
+  {key: 't', count: 2},
+  {key: 'f', count: 2},
+  {key: 'c', count: 2},
+  {key: 'g', count: 2},
+  {key: 'b', count: 2},
+  {key: 'd', count: 2}
+];
+
+const objectB = {value: ['a', 'd', 'e', 'f']};
+console.log(createUpdatedCollection(collectionA, objectB))
