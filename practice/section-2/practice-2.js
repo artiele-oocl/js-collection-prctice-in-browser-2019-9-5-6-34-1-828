51e8a35aeaa980b59855bf651c6091eafd0faf17
Lester Jan Artienda <artiele@oocl.com>
2019-09-12 17:30:15 +0800

    Complete practice-2-2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -4,16 +4,8 @@ function countSameElements(collection) {
   let out = [];
   collection.forEach((cv) => {
     const val = cv.split("");
-    if (val.length > 1) {
-      console.log("IF")
-      // iterate over val.
-      // key = val[0]
-      // count = iterate over val and find the number in it
-    } else {
-      countElements(out,cv)
-    }
+    val.length > 1 ? (out.push({"key":val[0],"count":getMultiplier(val)})) : countElements(out,cv);
   })
-
   return out;
 }
 
@@ -30,20 +22,10 @@ function countElements(out,key) {
 function isInObject(object,val){
   return object.some(ov => ov.key === val)
 }
-function isNumber(){
-  return;
+function getMultiplier(arr){
+  let multiplier;
+  arr.forEach(e => {
+    if(0/e === 0) multiplier = e;
+  })
+  return multiplier/1;
 }
-
-const collection = [
-  'a', 'a', 'a',
-  'e', 'e', 'e', 'e', 'e', 'e', 'e',
-  'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h',
-  't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-  'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f',
-  'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c',
-  'g', 'g', 'g', 'g', 'g', 'g', 'g',
-  'b', 'b', 'b', 'b', 'b', 'b',
-  'd-5'
-];
-
-console.log(countSameElements(collection));
