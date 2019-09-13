4de3e324e73eee79fec9e92cd802c6835d9fd182
Lester Jan Artienda <artiele@oocl.com>
2019-09-13 13:52:34 +0800

    Complete practice-3-2

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,15 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  return collectionA.map((value)=>{
+    return (isInObject(objectB,value.key)) ? ({"key":value.key, "count":value.count-getQuotient(value.count)}) : ({"key":value.key, "count":value.count})
+  })
+}
+
+function isInObject(object,val){
+  return object.value.indexOf(val) > -1
+}
+function getQuotient(num){
+  const quotient = Math.floor(num/3);
+  return quotient > 0 ? quotient : 1;
 }
