c2f81e6dd6f0a7fb497156f2e0db8ab9b7fa3aee
Lester Jan Artienda <artiele@oocl.com>
2019-09-13 18:22:41 +0800

    Refactor practice-1-1

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,16 +1,5 @@
 'use strict';
 
-// function collectSameElements(collectionA, collectionB) {
-//   let out = [];
-//   const pool = collectionA.concat(collectionB).sort();
-//   const filtered = pool.map((v,i)=>{
-//     if (v==pool[i+1]) out.push(v);
-//     return;
-//   })
-//   return out;
-  
-// }
-
 function collectSameElements(collectionA, collectionB) {
   let out = [];
   collectionA.forEach((av)=>{
