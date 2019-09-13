5c2b050069cf18e49f35336094abbb81874897c7
Lester Jan Artienda <artiele@oocl.com>
2019-09-13 17:58:47 +0800

    Complete practice-3-4

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -2,43 +2,49 @@
 
 function createUpdatedCollection(collectionA, objectB) {
   return getTotalCountOfEachKey(collectionA).map((value)=>{
-    return (isInObject(objectB,value.key)) ? ({"key":value.key, "count":value.count-getQuotient(value.count)}) : ({"key":value.key, "count":value.count})
+    return (isInObject(objectB.value,value.key)) ? ({"key":value.key, "count":value.count-getQuotient(value.count)}) : ({"key":value.key, "count":value.count})
   })
 }
 
+function getTotalCountOfEachKey(collectionA) {
+  let out = [];
+  const isInCollection = (object,val) => object.some(ov => ov.key === val);
+  collectionA.forEach(cv => {
+    const splitted = cv.split("");
+    if (splitted.length > 1) {
+      getTotalCountOfAlphanumericKey(out,splitted)
+    } else {
+      if (out.length === 0 || isInCollection(out,cv) === false) {
+        out.push({"key":cv,"count":1});
+      } else {
+        out.forEach(ov => {
+          if (ov.key==cv) ov.count++;
+        })
+      }
+    }
+  });
+  return out;
+}
+function getTotalCountOfAlphanumericKey(out,splitted) {
+  if (isInObject(out,splitted[0])) {
+    out.forEach(ov => {
+      if (ov.key==splitted[0]) ov.count+=getMultiplier(splitted);
+    })
+  } else {
+    out.push({"key":splitted[0],"count":getMultiplier(splitted)})
+  }
+}
+function getMultiplier(arr){
+  let multiplier=[];
+  arr.forEach(e => {
+    if(e/1 == e) multiplier.push(parseInt(e));
+  })
+  return parseInt(multiplier.toString().replace(",",""));
+}
 function isInObject(objectB,val){
-  return objectB.value.indexOf(val) > -1;
+  return objectB.indexOf(val) > -1;
 }
 function getQuotient(num){
   const quotient = Math.floor(num/3);
   return quotient > 0 ? quotient : 1;
 }
-function getTotalCountOfEachKey(collectionA) {
-  let out = [];
-  const isInCollection = (object,val) => object.some(ov => ov.key === val);
-  collectionA.forEach(cv => {
-    if (out.length === 0 || isInCollection(out,cv) === false) {
-      out.push({"key":cv,"count":1});
-    } else {
-      out.forEach(ov => {
-        if (ov.key==cv) ov.count++;
-      })
-    }
-  });
-  return out;
-}
-
-const collectionA = [
-  'a', 'a', 'a',
-  'e', 'e', 'e', 'e', 'e', 'e', 'e',
-  'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h', 'h',
-  't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't', 't',
-  'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f',
-  'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c',
-  'g', 'g', 'g', 'g', 'g', 'g', 'g',
-  'b', 'b', 'b', 'b', 'b', 'b',
-  'd-5'
-];
-const objectB = {value: ['a', 'd', 'e', 'f']};
-console.log(createUpdatedCollection(collectionA, objectB));
-
